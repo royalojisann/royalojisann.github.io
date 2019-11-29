4493e1f0efcce57e01fcbda39d8576d0210d51fc
royalojisann <57106739+royalojisann@users.noreply.github.com>
2019-11-30 05:35:18 +0900

    Update az_gatya_new_get.js

--- az_gatya_new_get.js
+++ az_gatya_new_get.js
@@ -391,6 +391,26 @@ function onedays(){
     document.getElementById("result_area").insertAdjacentHTML('afterbegin',txtdeta+'ほか:'+txtdeta_b);
     twion();
 }
+/**
+ * オブジェクト同士を加算
+ * @return Object 左辺と右辺を加算して作られた新しいオブジェクト
+ */
+Object.prototype.plus = function(arg){
+	var ret = {};
+	for(key in this){
+		ret[key] = this[key];
+	}
+	for(key in arg){
+		if(key in ret){
+			if(typeof arg[key] === 'function')continue;
+			ret[key] = ret[key] + arg[key];
+		}
+		else{
+			ret[key] = arg[key];
+		}
+	}
+	return ret;
+};
 
 function simulation(){
     hairetuset();
