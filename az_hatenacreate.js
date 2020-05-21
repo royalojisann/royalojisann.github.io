b3604497b91462bc2e9d97aea93fe2019af7d94d
royalojisann <57106739+royalojisann@users.noreply.github.com>
2020-05-22 07:06:57 +0900

    Update az_hatenacreate.js

--- az_hatenacreate.js
+++ az_hatenacreate.js
@@ -20,8 +20,6 @@ function status_in(){
     var target_area = document.getElementById("target_status");
     target_area.textContent="";
 
-    var get_level = document.getElementById("lebel_deta").value*1;
-    var get_kizuna = document.getElementById("kizuna").value*1;
     status_deta = {
         陣営:kan_list[index_n][0],
         艦種:kan_list[index_n][1],
@@ -51,90 +49,124 @@ function status_in(){
         対空砲座:kan_list[index_n][75],
         画像:kan_profile[index_n][10],
         スキル:kan_profile[index_n][11],
+        記事:kan_profile[index_n][12],
+        声優:kan_profile[index_n][8],
+        絵師:kan_profile[index_n][9],
+        入手:kan_profile[index_n][4],
+        実装日:kan_profile[index_n][5],
+        改造日:kan_profile[index_n][6],
+        進水日:kan_profile[index_n][7],
+        起工日:kan_profile[index_n][13],
+        竣工日:kan_profile[index_n][14],
     };
-    //耐久
-    var keisan = (kan_list[index_n][3]*1);
-        keisan+=(get_level-1)*(kan_list[index_n][4]*1)/1000;
-        keisan+=(Math.max(get_level,100)-100)*(kan_list[index_n][5]*1)/1000;
-        keisan+=(kan_list[index_n][57]*1);
-        keisan*=(get_kizuna);
-        keisan+=(kan_list[index_n][44]*1);
-        status_deta.耐久=Math.floor(keisan);
-    //装填
-    var keisan = (kan_list[index_n][18]*1);
-        keisan+=(get_level-1)*(kan_list[index_n][19]*1)/1000;
-        keisan+=(Math.max(get_level,100)-100)*(kan_list[index_n][20]*1)/1000;
-        keisan+=(kan_list[index_n][43]*1);
-        keisan+=(kan_list[index_n][62]*1);
-        keisan*=(get_kizuna);
-        keisan+=(kan_list[index_n][49]*1);
-        status_deta.装填=Math.floor(keisan);
-    //回避
-    var keisan = (kan_list[index_n][27]*1);
-        keisan+=(get_level-1)*(kan_list[index_n][28]*1)/1000;
-        keisan+=(Math.max(get_level,100)-100)*(kan_list[index_n][29]*1)/1000;
-        keisan+=(kan_list[index_n][64]*1);
-        keisan*=(get_kizuna);
-        keisan+=(kan_list[index_n][52]*1);
-        status_deta.回避=Math.floor(keisan);
-    //命中
-    var keisan = (kan_list[index_n][24]*1);
-    keisan+=(get_level-1)*(kan_list[index_n][25]*1)/1000;
-    keisan+=(Math.max(get_level,100)-100)*(kan_list[index_n][26]*1)/1000;
-    keisan+=(kan_list[index_n][63]*1);
-    keisan*=(get_kizuna);
-    keisan+=(kan_list[index_n][51]*1);
-    status_deta.命中=Math.floor(keisan);
-    //火力
-    var keisan = (kan_list[index_n][6]*1);
-    keisan+=(get_level-1)*(kan_list[index_n][7]*1)/1000;
-    keisan+=(Math.max(get_level,100)-100)*(kan_list[index_n][8]*1)/1000;
-    keisan+=(kan_list[index_n][39]*1);
-    keisan+=(kan_list[index_n][58]*1);
-    keisan*=(get_kizuna);
-    keisan+=(kan_list[index_n][45]*1);
-    status_deta.火力=Math.floor(keisan);
-    //雷装
-    var keisan = (kan_list[index_n][9]*1);
-    keisan+=(get_level-1)*(kan_list[index_n][10]*1)/1000;
-    keisan+=(Math.max(get_level,100)-100)*(kan_list[index_n][11]*1)/1000;
-    keisan+=(kan_list[index_n][40]*1);
-    keisan+=(kan_list[index_n][59]*1);
-    keisan*=(get_kizuna);
-    keisan+=(kan_list[index_n][46]*1);
-    status_deta.雷装=Math.floor(keisan);
-    //航空
-    var keisan = (kan_list[index_n][15]*1);
-    keisan+=(get_level-1)*(kan_list[index_n][16]*1)/1000;
-    keisan+=(Math.max(get_level,100)-100)*(kan_list[index_n][17]*1)/1000;
-    keisan+=(kan_list[index_n][42]*1);
-    keisan+=(kan_list[index_n][61]*1);
-    keisan*=(get_kizuna);
-    keisan+=(kan_list[index_n][48]*1);
-    status_deta.航空=Math.floor(keisan);
-    //対空
-    var keisan = (kan_list[index_n][12]*1);
-    keisan+=(get_level-1)*(kan_list[index_n][13]*1)/1000;
-    keisan+=(Math.max(get_level,100)-100)*(kan_list[index_n][14]*1)/1000;
-    keisan+=(kan_list[index_n][41]*1);
-    keisan+=(kan_list[index_n][60]*1);
-    keisan*=(get_kizuna);
-    keisan+=(kan_list[index_n][47]*1);
-    status_deta.対空=Math.floor(keisan);
-    //対潜
-    var keisan = (kan_list[index_n][36]*1);
-    keisan+=(get_level-1)*(kan_list[index_n][37]*1)/1000;
-    keisan+=(Math.max(get_level,100)-100)*(kan_list[index_n][38]*1)/1000;
-    keisan+=(kan_list[index_n][65]*1);
-    keisan*=(get_kizuna);
-    keisan+=(kan_list[index_n][55]*1);
-    status_deta.対潜=Math.floor(keisan);
-    //速力
-    var keisan = (kan_list[index_n][30]*1)+(kan_list[index_n][53]*1);
-    status_deta.速力 = keisan;
-    //幸運
-    var keisan = (kan_list[index_n][33]*1)+(kan_list[index_n][54]*1);
-    status_deta.幸運 = keisan;
+
+    switch(status_deta.記事){
+        case '暫定':
+            for(var pr=0; pr<pr_kansenn.length;pr++){
+                if(pr_kansenn[pr][2]==status_deta.名前){
+                    status_deta.耐久=(pr_kansenn[pr][3]*1);
+                    status_deta.火力=(pr_kansenn[pr][4]*1);
+                    status_deta.雷装=(pr_kansenn[pr][5]*1);
+                    status_deta.対空=(pr_kansenn[pr][6]*1);
+                    status_deta.航空=(pr_kansenn[pr][7]*1);
+                    status_deta.装填=(pr_kansenn[pr][8]*1);
+                    status_deta.命中=(pr_kansenn[pr][9]*1);
+                    status_deta.回避=(pr_kansenn[pr][10]*1);
+                    status_deta.対潜=(pr_kansenn[pr][11]*1);
+                    status_deta.幸運=(pr_kansenn[pr][12]*1);
+                    status_deta.速力=(pr_kansenn[pr][13]*1);
+                    break;
+                }
+            }
+        break;
+        default:
+            var get_level = document.getElementById("lebel_deta").value*1;
+            var get_kizuna = document.getElementById("kizuna").value*1;
+            //耐久
+            var keisan = (kan_list[index_n][3]*1);
+                keisan+=(get_level-1)*(kan_list[index_n][4]*1)/1000;
+                keisan+=(Math.max(get_level,100)-100)*(kan_list[index_n][5]*1)/1000;
+                keisan+=(kan_list[index_n][57]*1);
+                keisan*=(get_kizuna);
+                keisan+=(kan_list[index_n][44]*1);
+                status_deta.耐久=Math.floor(keisan);
+            //装填
+            var keisan = (kan_list[index_n][18]*1);
+                keisan+=(get_level-1)*(kan_list[index_n][19]*1)/1000;
+                keisan+=(Math.max(get_level,100)-100)*(kan_list[index_n][20]*1)/1000;
+                keisan+=(kan_list[index_n][43]*1);
+                keisan+=(kan_list[index_n][62]*1);
+                keisan*=(get_kizuna);
+                keisan+=(kan_list[index_n][49]*1);
+                status_deta.装填=Math.floor(keisan);
+            //回避
+            var keisan = (kan_list[index_n][27]*1);
+                keisan+=(get_level-1)*(kan_list[index_n][28]*1)/1000;
+                keisan+=(Math.max(get_level,100)-100)*(kan_list[index_n][29]*1)/1000;
+                keisan+=(kan_list[index_n][64]*1);
+                keisan*=(get_kizuna);
+                keisan+=(kan_list[index_n][52]*1);
+                status_deta.回避=Math.floor(keisan);
+            //命中
+            var keisan = (kan_list[index_n][24]*1);
+            keisan+=(get_level-1)*(kan_list[index_n][25]*1)/1000;
+            keisan+=(Math.max(get_level,100)-100)*(kan_list[index_n][26]*1)/1000;
+            keisan+=(kan_list[index_n][63]*1);
+            keisan*=(get_kizuna);
+            keisan+=(kan_list[index_n][51]*1);
+            status_deta.命中=Math.floor(keisan);
+            //火力
+            var keisan = (kan_list[index_n][6]*1);
+            keisan+=(get_level-1)*(kan_list[index_n][7]*1)/1000;
+            keisan+=(Math.max(get_level,100)-100)*(kan_list[index_n][8]*1)/1000;
+            keisan+=(kan_list[index_n][39]*1);
+            keisan+=(kan_list[index_n][58]*1);
+            keisan*=(get_kizuna);
+            keisan+=(kan_list[index_n][45]*1);
+            status_deta.火力=Math.floor(keisan);
+            //雷装
+            var keisan = (kan_list[index_n][9]*1);
+            keisan+=(get_level-1)*(kan_list[index_n][10]*1)/1000;
+            keisan+=(Math.max(get_level,100)-100)*(kan_list[index_n][11]*1)/1000;
+            keisan+=(kan_list[index_n][40]*1);
+            keisan+=(kan_list[index_n][59]*1);
+            keisan*=(get_kizuna);
+            keisan+=(kan_list[index_n][46]*1);
+            status_deta.雷装=Math.floor(keisan);
+            //航空
+            var keisan = (kan_list[index_n][15]*1);
+            keisan+=(get_level-1)*(kan_list[index_n][16]*1)/1000;
+            keisan+=(Math.max(get_level,100)-100)*(kan_list[index_n][17]*1)/1000;
+            keisan+=(kan_list[index_n][42]*1);
+            keisan+=(kan_list[index_n][61]*1);
+            keisan*=(get_kizuna);
+            keisan+=(kan_list[index_n][48]*1);
+            status_deta.航空=Math.floor(keisan);
+            //対空
+            var keisan = (kan_list[index_n][12]*1);
+            keisan+=(get_level-1)*(kan_list[index_n][13]*1)/1000;
+            keisan+=(Math.max(get_level,100)-100)*(kan_list[index_n][14]*1)/1000;
+            keisan+=(kan_list[index_n][41]*1);
+            keisan+=(kan_list[index_n][60]*1);
+            keisan*=(get_kizuna);
+            keisan+=(kan_list[index_n][47]*1);
+            status_deta.対空=Math.floor(keisan);
+            //対潜
+            var keisan = (kan_list[index_n][36]*1);
+            keisan+=(get_level-1)*(kan_list[index_n][37]*1)/1000;
+            keisan+=(Math.max(get_level,100)-100)*(kan_list[index_n][38]*1)/1000;
+            keisan+=(kan_list[index_n][65]*1);
+            keisan*=(get_kizuna);
+            keisan+=(kan_list[index_n][55]*1);
+            status_deta.対潜=Math.floor(keisan);
+            //速力
+            var keisan = (kan_list[index_n][30]*1)+(kan_list[index_n][53]*1);
+            status_deta.速力 = keisan;
+            //幸運
+            var keisan = (kan_list[index_n][33]*1)+(kan_list[index_n][54]*1);
+            status_deta.幸運 = keisan;
+        break;
+    }
         
     var status_in = 
     '<div class="maintagu_sute" style="font-weight: bold;"><table class="character_deta"><tbody>\n'+
