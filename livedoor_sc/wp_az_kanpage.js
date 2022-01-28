d65e2d5cee9f15ca8d1b4ff633b69f0efe901bf2
royalojisann <57106739+royalojisann@users.noreply.github.com>
2022-01-28 15:58:30 +0900

    Update wp_az_kanpage.js

--- livedoor_sc/wp_az_kanpage.js
+++ livedoor_sc/wp_az_kanpage.js
@@ -143,7 +143,7 @@ function kijicreate(){
 	profile[10].insertAdjacentHTML('beforeend' ,kan_profile[index_n][8]);
 	profile[11].insertAdjacentHTML('beforeend' ,kan_profile[index_n][9]);
 	profile[12].insertAdjacentHTML('beforeend' ,
-	'<img style="max-height: 100px;" src="'+("https://pasokau.com/wp-content/uploads/azpicture/"+teamslist[status_deta.陣営].フォルダ+"/"+status_deta.リンク+"/"+status_deta.リンク+"002.png")+'" class="lozad" alt="'+status_deta.正規名+'">'
+	'<img style="max-height: 100px;" src="'+("https://pasokau.com/wp-content/uploads/azpicture/"+teamslist[status_deta.陣営].フォルダ+"/"+status_deta.リンク+"/"+status_deta.リンク+"002.png")+'" alt="'+status_deta.正規名+'">'
 	);
 
 	let deletetext1= document.getElementById('kan_illust');
@@ -367,7 +367,7 @@ keisan = (kan_list[index_n][0] == 'META') ? (keisan+kan_list[index_n][55]*1)*get
             document.getElementsByClassName("maintagu_sute")[0].insertAdjacentHTML('beforeend',
             '<table class="status_deta" style="border-top: 1px solid #2196F3;border-bottom: 1px solid #2196F3;">\n'+
             '<thead>\n'+
-            '<tr style="border-bottom: 1px solid #2196F3;"><td class="picface '+status_deta.修正レア+'"><img src="'+status_deta.live[0]+'"></td><th class="'+status_deta.修正レア+'" colspan="5">'+status_deta.正規名+'</th></tr>\n'+
+            '<tr style="border-bottom: 1px solid #2196F3;"><td class="picface '+status_deta.修正レア+'"><img src="https://pasokau.com/wp-content/uploads/azpicture/'+teamslist[status_deta.陣営].フォルダ+'/'+status_deta.リンク+'/'+status_deta.リンク+'003.png"></td><th class="'+status_deta.修正レア+'" colspan="5">'+status_deta.正規名+'</th></tr>\n'+
             '</thead>\n'+
             '<tbody>\n'+
             '<tr><th>耐久</th><td>'+status_deta.耐久+'</td><th>装甲</th><td>'+status_deta.装甲+'</td><th>装填</th><td>'+status_deta.装填+'</td></tr>\n'+
