6b51d27b44162397c6ab125037fdd9108e4c8828
royalojisann <57106739+royalojisann@users.noreply.github.com>
2022-02-13 17:06:45 +0900

    Update koukokuset.js

--- livedoor_sc/koukokuset.js
+++ livedoor_sc/koukokuset.js
@@ -1,36 +1,20 @@
 ﻿window.addEventListener('DOMContentLoaded', function() {
-        var adine = "<div class='body_koukoku'><ins class='adsbygoogle'style='display:block; text-align:center;'data-ad-layout='in-article'data-ad-format='fluid'data-ad-client='ca-pub-1820058722476319'data-ad-slot='7802009807'></ins></div>";
+        //var adine = "<div class='body_koukoku'><ins class='adsbygoogle'style='display:block; text-align:center;'data-ad-layout='in-article'data-ad-format='fluid'data-ad-client='ca-pub-1820058722476319'data-ad-slot='7802009807'></ins></div>";
 	var adine_top = "<ins class='adsbygoogle'style='display:block; text-align:center;'data-ad-layout='in-article'data-ad-format='fluid'data-ad-client='ca-pub-1820058722476319'data-ad-slot='7802009807'></ins>";
 	var contentkoukoku = "<ins class='adsbygoogle' style='display:block' data-ad-format='autorelaxed' data-ad-client='ca-pub-1820058722476319' data-ad-slot='7882308349'></ins>";
-
-//     if(document.getElementById("koukoku_top")!=null){
-//         document.getElementById("koukoku_top").insertAdjacentHTML('beforeend',adine_top);
-//         (adsbygoogle = window.adsbygoogle || []).push({});
-//     }
-//         if(document.getElementById("koukoku_bottom")!=null){
-//             document.getElementById("koukoku_bottom").insertAdjacentHTML('beforeend',adine);
-//             (adsbygoogle = window.adsbygoogle || []).push({});
-//         }
-if(document.getElementById("sub_koukoku")!=null){
-        document.getElementById("sub_koukoku").insertAdjacentHTML('beforeend',adine);
-        (adsbygoogle = window.adsbygoogle || []).push({});
-    }
-if(document.getElementById("sub_koukoku1")!=null){
-        document.getElementById("sub_koukoku1").insertAdjacentHTML('beforeend',adine);
-        (adsbygoogle = window.adsbygoogle || []).push({});
-    }
-if(document.getElementById("sub_koukoku2")!=null){
-        document.getElementById("sub_koukoku2").insertAdjacentHTML('beforeend',adine);
-        (adsbygoogle = window.adsbygoogle || []).push({});
-    }
-if(document.getElementById("sub_koukoku3")!=null){
-        document.getElementById("sub_koukoku4").insertAdjacentHTML('beforeend',adine);
-        (adsbygoogle = window.adsbygoogle || []).push({});
-    }
-if(document.getElementById("contentdiv")!=null){
-        document.getElementById("contentdiv").insertAdjacentHTML('beforeend',contentkoukoku);
-        (adsbygoogle = window.adsbygoogle || []).push({});
-    }
+	 
+	 if(document.getElementById("koukoku_top")!=null){
+		 document.getElementById("koukoku_top").insertAdjacentHTML('beforeend',adine_top);
+		 (adsbygoogle = window.adsbygoogle || []).push({});
+	 }
+	 if(document.getElementById("koukoku_bottom")!=null){
+		 document.getElementById("koukoku_bottom").insertAdjacentHTML('beforeend',adine);
+		 (adsbygoogle = window.adsbygoogle || []).push({});
+	 }
+	 if(document.getElementById("contentdiv")!=null){
+		 document.getElementById("contentdiv").insertAdjacentHTML('beforeend',contentkoukoku);
+		 (adsbygoogle = window.adsbygoogle || []).push({});
+	 }
 })
 
 function switch_table_display(classname,number_data,dis){
